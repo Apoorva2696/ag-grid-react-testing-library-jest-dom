258f9271b5643b11d5eac5d4505f9fa5bcd99946
Apoorva2696 <apoorva2696@gmail.com>
2020-11-13 22:37:44 +0530

    test cases

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import axios from 'axios';
 import { AgGridReact } from '@ag-grid-community/react';
-import { ServerSideRowModelModule } from '@ag-grid-enterprise/all-modules';
+import { ColumnsToolPanelModule, ServerSideRowModelModule } from '@ag-grid-enterprise/all-modules';
 import '@ag-grid-community/core/dist/styles/ag-grid.css';
 import '@ag-grid-community/core/dist/styles/ag-theme-alpine-dark.css';
 
@@ -13,8 +13,8 @@ export default class GridExample extends Component {
     super(props);
 
     this.gridOptions = {
-      modules: [ServerSideRowModelModule],
-      columnDefs: [
+      modules: [ServerSideRowModelModule, ColumnsToolPanelModule],
+      columnDefs: props.columnDefs ? props.columnDefs :[
         {
           field: 'athlete',
           minWidth: 220,
@@ -27,17 +27,25 @@ export default class GridExample extends Component {
         {
           field: 'sport',
           minWidth: 200,
-        }
+        },
+        { field: 'gold' },
+        { field: 'silver' },
+        { field: 'bronze' }
       ],
       defaultColDef: {
         flex: 1,
         minWidth: 100,
       },
       rowModelType: 'serverSide',
+      pagination: true,
+      paginationPageSize: props.pageSize? props.pageSize:10
     };
   }
 
   onGridReady = (params) => {
+
+    //sample data to be used only for testing
+    const { sampleData } = this.props;
 
     /**incase of functional components you can use const gridApi =useRef()
      * and set gridApi.current = params.api in onGridReady  
@@ -48,18 +56,20 @@ export default class GridExample extends Component {
     const updateData = () => {
 
       // you can eliminate the fake server when fetching actual data
-      var fakeServer = createFakeServer();
-      var datasource = createServerSideDatasource(fakeServer);
+      const fakeServer = createFakeServer(sampleData);
+
+      //data source is used by ag grid to fetch 
+      const datasource = createServerSideDatasource(fakeServer);
       params.api.setServerSideDatasource(datasource);
     };
 
-    updateData()
-
+    updateData();
   };
 
   render() {
     return (
-      <div style={{ width: '100%', height: '100%' }}>
+      <div style={{ width: '100%', height: '520px' }}>
+        {this.gridApi !== undefined ? <div data-testid={'grid-ready'}/>:null}
         <div
           id="myGrid"
           style={{
@@ -68,6 +78,7 @@ export default class GridExample extends Component {
           }}
           className="ag-theme-alpine-dark"
         >
+          
           <AgGridReact
             {...this.gridOptions}
             onGridReady={this.onGridReady}
@@ -80,30 +91,32 @@ export default class GridExample extends Component {
 
 function createServerSideDatasource(server) {
   return {
-    getRows: function (params) {
+    getRows: async function (params) {
      
-      const rawResponse = axios.post(
+      const {data} = await axios.get(
         'https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/olympicWinners.json'
-      , params.request);
-
-      // no need to call server if you are fetching data from actual server; fake server is just for this example
-      var response = server.getData(JSON.parse(rawResponse), params.request);
+      );
       
-        if (response.success) {
+      // no need to call server if you are fetching data from actual server; fake server is just for this example
+      const _response = server.getData(data, params.request);
+      
+        if (_response) {
           // rows gets populated and last row is used for pagination by ag grid
-          params.successCallback(response.rows, response.lastRow);
+          params.successCallback(_response.rows, _response.lastRow);
         } else {
           params.failCallback();
+          this.gridApi.showNoRowsOverlay();
         }
       
     }
   };
 }
-function createFakeServer(allData) {
+function createFakeServer(sampleData) {
   return {
-    getData: function (request) {
-      var requestedRows = allData.slice(request.startRow, request.endRow);
-      var lastRow = getLastRowIndex(request, requestedRows);
+    getData: function (data, request) {
+      const requestedRows = sampleData ? sampleData:data.slice(request.startRow, request.endRow);
+      const lastRow = getLastRowIndex(request, requestedRows);
+      console.log(requestedRows)
       return {
         success: true,
         rows: requestedRows,
@@ -114,8 +127,7 @@ function createFakeServer(allData) {
 }
 function getLastRowIndex(request, results) {
   if (!results) return undefined;
-  var currentLastRow = request.startRow + results.length;
+  const currentLastRow = request.startRow + results.length;
   return currentLastRow < request.endRow ? currentLastRow : undefined;
 }
 
-render(<GridExample></GridExample>, document.querySelector('#root'));
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,49 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+const colDefs = [
+  { field: 'name', headerName: 'Name' },
+  { field: 'age', headerName: 'Age' }
+];
+
+const rowData = [
+  { name: 'John', age: 30 },
+  { name: 'Rob', age: 26 }
+];
+
+const gridOptions = {
+  pageSize: 2
+};
+
+describe('Server driven ag grid', () => {
+  let grid = null;
+
+  beforeEach( async () => {
+    grid = render(<App {...gridOptions} sampleData = {rowData} columnDefs={colDefs}/>);
+    const {queryByTestId} = grid;
+    await waitFor(() => queryByTestId('grid-ready') !== null)
+  })
+
+  afterEach(() => {
+    const { unmount } = grid;
+    unmount();
+  })
+
+  it('renders given columns', () => {
+    expect(screen.getByText('Name')).toBeInTheDocument();
+    expect(screen.getByText('Age')).toBeInTheDocument();
+  })
+
+  // it('renders given rows', () => {
+  //   expect(screen.getByText('John')).toBeInTheDocument();
+  //   expect(screen.getByText('30')).toBeInTheDocument();
+  //   expect(screen.getByText('Rob')).toBeInTheDocument();
+  //   expect(screen.getByText('26')).toBeInTheDocument();
+  // })
+
+  it('render rows', () => {
+    expect(screen.getAllByRole('row').length).toBe(4)
+  })
+
+} );
